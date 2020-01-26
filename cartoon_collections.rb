8a8a1912a4438d4f5d680f8ef963f8582652d66b
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-26 06:15:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,13 @@ def summon_captain_planet(arr)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  new_arr = []
+  arr.each do |element|
+    if element.length > 4 
+      new_arr << element
+    end
+    return new_arr
 end
 
 def find_the_cheese# code an argument here
