cc65c229d7b3417ce88f749ef753f4be03e0373e
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-26 06:20:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,6 @@ end
 
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.find{|element| element ===""}
+  arr.find{|element| element ==="cheddar" || "gouda" || "camembert"
+  }
 end
